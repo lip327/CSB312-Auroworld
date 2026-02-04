b48bbbcd82b22f5a3d00af90ea700c26d705fc2a
unknown <lip327@lehigh.edu>
2026-02-04 16:21:04 -0500

    added google token class

---
+++ backend/src/main/java/auroWorld/backend/googletoken.js
@@ -0,0 +1,50 @@
+/** JSON returned by Google's tokeninfo endpoint (subset) */
+    // private static final class GoogleTokenInfo {
+    //     public String aud;
+    //     public String iss;
+    //     public String email;
+    //     public String hd;
+    //     public String sub;
+    //     public String given_name;
+    //     public String family_name;
+    //     public String email_verified;
+    // }
+//class for google token for oauth
+export class GoogleToken{
+
+    constructor(aud, iss, email, hd, sub, given_name, family_name, email_verified){
+        this.aud=aud;
+        this.iss=iss;
+        this.email=email;
+        this.hd=hd;
+        this.sub=sub;
+        this.given_name=given_name;
+        this.family_name=family_name;
+        this.email_verified=email_verified;
+    }
+    getAud(){
+        return this.aud;
+    }
+    getIss(){
+        return this.iss;
+    }
+    getEmail(){
+        return this.email;
+    }
+    getHd(){
+        return this.hd;
+    }
+    getSub(){
+        return this.sub;
+    }
+    getGivenName(){
+        return this.given_name;
+    }
+    getFamilyName(){
+        return this.family_name;
+    }
+    getEmailVerified(){
+        return this.email_verified;
+    }
+}
+
